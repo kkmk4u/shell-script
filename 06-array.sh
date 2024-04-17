138b8259cdbc7102fb6901980134979e92a7f0e7
kkmk4u <k.madhukiran.4u@gmail.com>
2024-04-18 02:52:25 +0530

    start message

---
+++ 06-array.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+MOVIES=("RRR" "DJ Tillu" "Murari")
+
+#size of above array is 3
+
+#index are 0,1,2
+
+# list always start with 0
+
+echo "First Movie is : ${MOVIES[0]}"
+echo "second Movie is :${MOVIES[1]]}"
+echi "threed Movie is :${MOVIES[2]}"
+echi "All Movies is :${MOVIES[@]}"
