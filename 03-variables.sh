dac66a781c1c7108f76f5668071877b81c2a4884
kkmk4u <k.madhukiran.4u@gmail.com>
2024-04-18 01:05:09 +0530

    hello script

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+Peron1=Ramesh
+Person2=suresh
+
+
+echo "$Peron1:: hello $Peron2; how are you?"
+echo "$Peron2:: hi $Peron1, i am fine. how are you?"
+echo "$Peron1:: i am fine too. how is your work?"
+echo "$Peron2:: not bad. i am thinking to upgrade to devops"
