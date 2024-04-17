b866c47e51f660b76224b5c8c595a19057133d38
kkmk4u <k.madhukiran.4u@gmail.com>
2024-04-18 00:56:14 +0530

    hello script

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,1 +1,6 @@
-this is again sample script
+#!/bin/bash
+
+echo "Ramesh:: hello suresh; how are you?"
+echo "suresh:: hi ramesh, i am fine. how are you?"
+echo "ramesh:: i am fine too. how is your work?"
+echo "suresh:: not bad. i am thinking to upgrade to devops"
