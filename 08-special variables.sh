45c21f5b3491734308c20dd7f08bfd3b7ac17845
kkmk4u <k.madhukiran.4u@gmail.com>
2024-04-19 01:01:37 +0530

    start message

--- 08-special variables.sh
+++ 08-special variables.sh
@@ -3,4 +3,5 @@
 echo "All variables: $@"
 echo "Number of variables passed: $#"
 echo "Script Name: $0"
-echo "Currentworking directory: $"
+echo "Currentworking directory: $PWD"
+echo "Home directory of current user: $HOME"
